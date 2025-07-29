39f650a4dc24f47005922a10d58d5715d7ce588d
Adrien <adrienbaptiste@live.fr>
2025-07-29 21:32:03 +0200

    feat: protection de la preprod

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthProvider';
+import PreprodProtection from './components/organisms/PreprodProtection';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <PreprodProtection>
+        <App />
+      </PreprodProtection>
     </AuthProvider>
   </React.StrictMode>
 );
